915c8ecd9e003c8803049533862e7507c5d781f5
Eliam <eliam.toye@student.vives.be>
2024-03-30 15:29:48 +0100

    userpage en confirm page testen

---
+++ Pages/ConfirmPage.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace Project.Pages
+{
+    public class ConfirmModel : PageModel
+    {
+        [BindProperty]
+        public List<string> Lessons { get; set; }
+
+        public void OnGet(List<string> selectedLessons)
+        {
+            Lessons = selectedLessons;
+        }
+
+        public IActionResult OnPostSubmit()
+        {
+            // Process the form submission here
+            // Redirect or perform any necessary action
+
+            return RedirectToPage("/UserPage"); // Redirect to the UserPage after submission
+        }
+    }
+}
---
+++ Pages/UserPage.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+
+namespace Project.Pages
+{
+    public class UserPageModel : PageModel
+    {
+        // Your model properties, if any
+
+        public void OnGet()
+        {
+            // Handle GET request if necessary
+        }
+
+        public IActionResult OnPost()
+        {
+            // Check if any checkboxes are checked
+            var selectedLessons = Request.Form["lessons[]"].ToList();
+
+            if (selectedLessons.Any())
+            {
+                // Redirect to the Confirm page
+                return RedirectToPage("/Confirm", new { SelectedLessons = selectedLessons });
+            }
+            else
+            {
+                // No checkboxes selected, just refresh the page
+                return Page();
+            }
+        }
+    }
+}
